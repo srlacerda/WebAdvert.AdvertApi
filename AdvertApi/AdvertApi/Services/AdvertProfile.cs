372fdb5b68109071c7803e9a77b330e2496de5e8
srlacerda <diego.lacerda.alves@gmail.com>
2020-04-28 13:29:56 -0300

    Criada tabela Advert no Dynamo

---
+++ AdvertApi/AdvertApi/Services/AdvertProfile.cs
@@ -0,0 +1,18 @@
+﻿using AdvertApi.Models;
+using AutoMapper;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace AdvertApi.Services
+{
+    public class AdvertProfile : Profile
+    {
+        public AdvertProfile()
+        {
+            //CreateMap<AdvertDbModel, AdvertModel>().ReverseMap();
+            CreateMap<AdvertModel, AdvertDbModel>();
+        }
+    }
+}
---
+++ AdvertApi/AdvertApi/Services/DynamoDBAdvertStorage.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using AdvertApi.Models;
+using AutoMapper;
+using Amazon.DynamoDBv2;
+using Amazon.DynamoDBv2.DataModel;
+
+namespace AdvertApi.Services
+{
+    public class DynamoDBAdvertStorage : IAdvertStorageService
+    {
+        private readonly IMapper _mapper;
+        public DynamoDBAdvertStorage(IMapper mapper)
+        {
+            _mapper = mapper;
+        }
+        public async Task<string> Add(AdvertModel model)
+        {
+            var dbModel = _mapper.Map<AdvertDbModel>(model);
+            dbModel.Id = new Guid().ToString();
+            dbModel.CreationDateTime = DateTime.UtcNow;
+            dbModel.Status = AdvertStatus.Pending;
+
+            using (var client = new AmazonDynamoDBClient())
+            {
+                using (var context = new DynamoDBContext(client))
+                {
+                    await context.SaveAsync(dbModel);
+                }
+            }
+            return dbModel.Id;
+        }
+
+        public Task<bool> Confirm(ConfirmAdvertModel model)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
