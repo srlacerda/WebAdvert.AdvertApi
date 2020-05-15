94c8d5e53f6bca75207f4ea92c0284671067cdcb
srlacerda <diego.lacerda.alves@gmail.com>
2020-05-15 04:30:19 -0300

    Microservice Discovery

--- AdvertApi/AdvertApi/Startup.cs
+++ AdvertApi/AdvertApi/Startup.cs
@@ -12,6 +12,10 @@ using Microsoft.Extensions.Options;
 using AutoMapper;
 using AdvertApi.Services;
 using AdvertApi.HealthChecks;
+using Microsoft.OpenApi.Models;
+using Amazon.Util;
+using Amazon.ServiceDiscovery;
+using Amazon.ServiceDiscovery.Model;
 
 namespace AdvertApi
 {
@@ -46,10 +50,27 @@ namespace AdvertApi
                 options.AddPolicy("AllOrigin", policy => policy.WithOrigins("*").AllowAnyHeader());
             });
 
+            services.AddSwaggerGen(options =>
+            {
+                options.SwaggerDoc("v1", new OpenApiInfo
+                {
+                    Title = "Web Advertisement Apis",
+                    Version = "version 1",
+                    Contact = new OpenApiContact
+                    {
+                        Name = "Diego Lacerda",
+                        Email = "diego.lacerda.alves@gmail.com"
+                    }
+                });
+            });
+
+            //services.UseCustomCloudMapClient<CloudMapClient>();
+
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app, IHostingEnvironment env)
+        //public async Task Configure(IApplicationBuilder app, IHostingEnvironment env)
         {
             if (env.IsDevelopment())
             {
@@ -57,8 +78,37 @@ namespace AdvertApi
             }
 
             app.UseHealthChecks("/health");
+            app.UseSwagger();
+            app.UseSwaggerUI(c =>
+            {
+                c.SwaggerEndpoint("/swagger/v1/swagger.json", "Web Advert Api");
+            });
+
+            //funcionando HARDCODE
+            //RegisterToCloudMap();
 
             app.UseMvc();
+        }
+
+        private async Task RegisterToCloudMap()
+        {
+            const string serviceId = "srv-mddqavg6dnwc4x2l";
+            //var instanceID = EC2InstanceMetadata.InstanceId;
+            var instanceID = "i-03277659830d791cf";
+
+            if (!string.IsNullOrEmpty(instanceID))
+            {
+                //var ipv4 = EC2InstanceMetadata.PrivateIpAddress;
+                var ipv4 = "18.218.240.116";
+                var client = new AmazonServiceDiscoveryClient();
+                await client.RegisterInstanceAsync(new RegisterInstanceRequest
+                {
+                    InstanceId  = instanceID,
+                    ServiceId = serviceId,
+                    Attributes = new Dictionary<string, string>() { {"AWS_INSTANCE_IPV4",ipv4 },
+                        { "AWS_INSTANCE_PORT","80"} }
+                }).ConfigureAwait(false);
+            }
         }
     }
 }
